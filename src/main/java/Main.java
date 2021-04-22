f3049c8347d8688269ffa7ff6447f709b3b28f3e
Ada Michocka <amichocka@wp.pl>
2021-04-22 21:39:34 +0200

    add scientific notation to zad 2

---
+++ src/main/java/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Zad_1 zad_1;
+        Zad_2 zad_2;
+        Zad_3 zad_3;
+        Zad_4 zad_4;
+        Zad_5 zad_5;
+        Scanner scanner = new Scanner(System.in);
+
+        boolean war = false;
+        while (war == false) {
+            System.out.println("Wpisz nr zad");
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+
+            }
+        }
+
+    }
+}
--- src/main/java/Zad_2.java
+++ src/main/java/Zad_2.java
@@ -1,4 +1,8 @@
 import java.math.BigInteger;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class Zad_2 {
 
@@ -7,7 +11,7 @@ public class Zad_2 {
         //Zastosowanie typów prostych uniemożliwiłoby otrzymanie tak dużej wartości silni.
         //jeśli zawali ci kompa to zmniejsz n
 
-        int n = 1000000;
+        int n = 100000;
         BigInteger silnia = BigInteger.ONE;
         long startTime = System.nanoTime();
         for (int i = 1; i < n; i++) {
@@ -16,8 +20,10 @@ public class Zad_2 {
         }
 
         long estimatedTime = System.nanoTime() - startTime;
+        NumberFormat formatter = new DecimalFormat("0.######E0", DecimalFormatSymbols.getInstance(Locale.ROOT));
+        String str = formatter.format(silnia);
 
-        System.out.println("Silnia o n: " + n + " wynosi: " + silnia);
+        System.out.println("Silnia o n: " + n + " wynosi: " + str);
         System.out.println("wyznaczona w czasie: " + estimatedTime);
     }
 
